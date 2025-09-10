737d35573184f394fcb4fc1c9d676fdf56dcffa9
tshives26 <tshives26@gmail.com>
2025-09-09 23:51:57 -0600

    Update build.gradle.kts with signing configuration changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.chilluminati.rackedup"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
